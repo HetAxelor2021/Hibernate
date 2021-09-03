d979abe16e9770801fdee9ed1d37841270f179c3
HetAxelor2021 <hch.axelor@gmail.com>
2021-09-03 14:51:48 +0530

    persistant life cycle

--- HQLProject1/src/main/java/com/hh/HQLProj1/App.java
+++ HQLProject1/src/main/java/com/hh/HQLProj1/App.java
@@ -1,6 +1,5 @@
 package com.hh.HQLProj1;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -8,21 +7,96 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 public class App {
 	
 	public static void main(String[] args) {
-		Configuration  config = new Configuration().configure().addAnnotatedClass(Student.class);
+		
+		
+//		Laptop laptop = new Laptop();
+//		laptop.setBrand("dell");
+//		laptop.setId(1);
+//		laptop.setPrice(1000);
+//		
+//		
+//		
+		
+		
+		
+		
+		
+		
+		
+		Configuration  config = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
 //		ServiceRegistry registry = new ServiceRegistryBuilder().applySetting(config.getProperties()).buildServiceRegistry();
 		SessionFactory sf = config.buildSessionFactory();
 		Session session  = sf.openSession();
-		int b= 60;
 		session.beginTransaction();
-		Query q = session.createQuery(" select sum(marks) from Student where marks>:b");
-		q.setParameter("b", b);
-		Long marks = (Long) q.uniqueResult();
-		System.out.println(marks);
+		
+		Laptop l= new Laptop();
+		l.setId(55);
+		l.setBrand("Lenovo");
+		l.setPrice(900);
+		session.save(l);
+//		l.setPrice(750);
+		session.remove(l);
+		
+		session.getTransaction().commit();
+//		session.detach(l);
+//		l.setPrice(600);
+		
+		session.close();
+		
+		
+		
+		
+		
+		
+//		Random r= new Random();
+//		int b= 60;
+//		Query q = session.createQuery(" select sum(marks) from Student where marks>:b");
+//		q.setParameter("b", b);
+//		Long marks = (Long) q.uniqueResult();
+//		System.out.println(marks);
+//		
+//		System.out.println("hi");
+//		session.save(laptop);
+//		Query q = session.createSQLQuery("select * from laptop");
+//		List<Laptop> l = q.getResultList();
+//		System.out.println(l);
+//		for(int i=1;i<=50;i++) {
+//			Laptop laptop = new Laptop();
+//			laptop.setId(i);
+//			laptop.setBrand("brand "+i);
+//			laptop.setPrice(r.nextInt(1000));
+//			session.save(laptop);
+//			
+//		}
+//		
+//		
+//		//native query
+//		SQLQuery query = session.createSQLQuery("select name,marks from student where marks>60");
+//		
+//		query.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+//		List students  = query.list();
+//		for(Object o : students){
+//			Map m= (Map)o;
+//			System.out.println(m.get("name")+" : "+m.get("marks"));
+//		}
+		
+		
+		
+//		query.addEntity(Student.class);
+//		
+//		List<Student> students = query.list();
+//		
+//		for(Student o : students) {
+//			System.out.println(o);
+//		}
+		
+		
+		
+		
 		
 //		Query q = session.createQuery("select rollno from Student where marks=50");
 //		Integer ans  = (Integer)q.uniqueResult();
@@ -62,6 +136,5 @@ public class App {
 //			session.save(s);
 //		}
 //		
-		session.getTransaction().commit();
 	}
 }
--- HQLProject1/src/main/java/com/hh/HQLProj1/Laptop.java
+++ HQLProject1/src/main/java/com/hh/HQLProj1/Laptop.java
@@ -1,8 +1,38 @@
 package com.hh.HQLProj1;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="laptop")
 public class Laptop {
-
+	@Id
+	private int id;
+	private String brand;
+	private int price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Laptop [id=" + id + ", brand=" + brand + ", price=" + price + "]";
+	}
+	
+	
 }
