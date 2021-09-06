915d19c565bf5583000ab1bc4c2a7d80c4da3782
HetAxelor2021 <hch.axelor@gmail.com>
2021-09-06 23:22:39 +0530

    manytomany implementation update

--- LaptopStudentProj/src/main/java/com/hh/LaptopStudentProjEx/Student.java
+++ LaptopStudentProj/src/main/java/com/hh/LaptopStudentProjEx/Student.java
@@ -20,7 +20,7 @@ public class Student {
 	
 	private int marks;
 	
-	@OneToMany(cascade = CascadeType.ALL)
+	@ManyToMany(cascade = CascadeType.ALL)
 	private List<Laptop> laptop = new ArrayList<Laptop>();
 	
 	
